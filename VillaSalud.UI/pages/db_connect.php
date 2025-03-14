076bf853ae01e1aacf5518149fda9acfee415703
Angelica G. Rosario <angelicalykarosario@gmail.com>
2025-03-15 04:29:15 +0800

    fix db connection

--- VillaSalud.UI/pages/db_connect.php
+++ VillaSalud.UI/pages/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";  
 $user = "root";       
-$pass = "marcgasta1902.";  //change into the password na meron ka     
+$pass = "rosario09876";  //change into the password na meron ka     
 $dbname = "Salud"; 
 
 $conn = new mysqli($host, $user, $pass, $dbname);
