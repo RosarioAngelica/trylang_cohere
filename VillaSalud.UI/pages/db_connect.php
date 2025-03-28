28bf9657cfc1352e0fb55f5e712b1c5e7e71b113
Gasta Marc Oliver <marcolivergastagonzales@gmail.com>
2025-03-29 02:50:49 +0800

    chnage pass on db connection

--- VillaSalud.UI/pages/db_connect.php
+++ VillaSalud.UI/pages/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";  
 $user = "root";       
-$pass = "marcgasta1902.";  //change into the password na meron ka  + rosario09876 + marcgasta1902. +
+$pass = "rosario09876";  //change into the password na meron ka  + rosario09876 + marcgasta1902. +
 $dbname = "Salud"; 
 
 $conn = new mysqli($host, $user, $pass, $dbname);
