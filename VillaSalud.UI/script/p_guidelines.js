0c13b0907e932482e3cf996aa8fd9b8e951116f2
Angelica G. Rosario <angelicalykarosario@gmail.com>
2025-06-08 03:48:13 +0800

    add patron guidelines

---
+++ VillaSalud.UI/script/p_guidelines.js
@@ -0,0 +1,10 @@
+// Smooth scrolling to internal anchors if used (future-proof)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
